f78c2ea45aa753d800310167917a1bccca11cf0e
siuari <siuaridamaceno@gmail.com>
2025-08-30 13:51:31 -0300

    feat(Log): Configuração de Sorilog

---
+++ src/AnimeHub.Api/Middlewares/GlobalExceptionHandler.cs
@@ -0,0 +1,48 @@
+﻿using AnimeHub.Domain.DomainExceptions;
+
+namespace AnimeHub.Api.Middlewares
+{
+    public class GlobalExceptionHandler
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalExceptionHandler> _logger;
+
+        public GlobalExceptionHandler(RequestDelegate next, ILogger<GlobalExceptionHandler> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (AnimeHubValidationException ex)
+            {
+                _logger.LogWarning(ex, "Validation error occurred.");
+                context.Response.StatusCode = 400;
+                context.Response.ContentType = "application/json";
+                var response = new
+                {
+                    Message = ex.Message
+                };
+
+                await context.Response.WriteAsJsonAsync(response);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Ocorreu um erro interno do servidor.");
+                context.Response.StatusCode = 500;
+                context.Response.ContentType = "application/json";
+                var response = new
+                {
+                    Message = "Ocorreu um erro interno do servidor. Por favor, tente novamente"
+                };
+
+                await context.Response.WriteAsJsonAsync(response);
+            }
+        }
+    }
+}
--- src/AnimeHub.Api/Program.cs
+++ src/AnimeHub.Api/Program.cs
@@ -1,32 +1,61 @@
 using AnimeHub.Api.Configurations;
+using AnimeHub.Api.Middlewares;
 using AnimeHub.Infra.IoC;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 
-// Add services to the container
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-builder.Services.RegisterServices();
-
-builder.Services.AddDbContextConfiguration(builder.Configuration);
-
-builder.Services
-    .AddMediatR(cfg => cfg.RegisterServicesFromAssembly(AppDomain.CurrentDomain.Load("AnimeHub.Application")));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.MapOpenApi();
+    Log.Information("starting server.");
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services
+        .AddSerilog((services, lc) => lc
+           .ReadFrom.Configuration(builder.Configuration)
+           .ReadFrom.Services(services)
+           .Enrich.FromLogContext()
+           .WriteTo.Console());
+
+    // Add services to the container
+    builder.Services.AddControllers();
+    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+    builder.Services.AddOpenApi();
+
+    builder.Services.RegisterServices();
+
+    builder.Services.AddDbContextConfiguration(builder.Configuration);
+
+    builder.Services
+        .AddMediatR(cfg => cfg.RegisterServicesFromAssembly(AppDomain.CurrentDomain.Load("AnimeHub.Application")));
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.UseSerilogRequestLogging();
+
+    app.UseMiddleware<GlobalExceptionHandler>();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application start-up failed");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
